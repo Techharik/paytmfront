333bed8d27dfb2d277417a9bb02a523010435aa4
Hari Prasath <khariprasath30@gmail.com>
2024-11-21 17:07:27 +0530

    setting up the signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './routes/SignUp'
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/signup' element={<SignUp />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
