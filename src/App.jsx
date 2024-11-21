5885c96018e7243934ff2054abf4b8f0387e42a0
Hari Prasath <khariprasath30@gmail.com>
2024-11-21 19:41:34 +0530

    signup page is added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path='/signup' element={<SignUp />} />
+                    <Route path='/singin' element={<SignUp />} />
                 </Routes>
             </BrowserRouter>
         </>
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate, } from 'react-router-dom'
+import axios from 'axios'
+
 
 const SignUp = () => {
-    return (
-        <div>
+    const [formDetails, setformDetails] = useState({
+        username: '',
+        password: '',
+        firstname: "",
+        lastname: ""
+    })
+    const navigate = useNavigate()
 
+
+    const handlechange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value)
+        setformDetails({ ...formDetails, [name]: value })
+    }
+
+    const handleSubmit = async () => {
+        try {
+
+            const response = await axios.post('http://localhost:3000/api/v1/registeruser', formDetails)
+            console.log(response)
+            if (response.data.success == true) {
+                localStorage.setItem('tokenIn', response.data.data);
+                navigate('/dashboard')
+            }
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+
+    return (
+        <div className='h-screen w-full flex justify-center items-center'>
+            <div className='bg-white p-4 rounded-lg'>
+                <div className='flex flex-col items-center'>
+                    <h1 className='font-semibold text-lg'>Sign Up</h1>
+                    <p className='text-slate-400 text-center w-72 pb-8'>Enter your authentication to  create your account</p>
+                </div>
+
+                <div className='w-[400px] px-10'>
+                    <div className='flex flex-col py-2'>
+                        <label htmlFor="firstname">First Name</label>
+                        <input type="text" placeholder='John' className='border p-2 rounded-lg mt-1'
+                            value={formDetails.firstname}
+                            name='firstname'
+                            onChange={(e) => handlechange(e)}
+                        />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label htmlFor="lastname">Last Name</label>
+                        <input type="text" placeholder='Doe' className='border p-2 rounded-lg mt-1'
+                            value={formDetails.lastname}
+                            name='lastname'
+                            onChange={(e) => handlechange(e)}
+                        />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder='hari@gmail.com' className='border p-2 rounded-lg mt-1'
+                            value={formDetails.username}
+                            name='username'
+                            onChange={(e) => handlechange(e)}
+                        />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder='hari@123#' className='border p-2 rounded-lg mt-1'
+                            value={formDetails.password}
+                            name='password'
+                            onChange={(e) => handlechange(e)}
+                        />
+                    </div>
+
+                    <button className='w-full bg-black  text-white font-semibold py-2 mt-6 rounded-lg hover:bg-opacity-80'
+                        onClick={handleSubmit}
+                    >
+                        Sign up
+                    </button>
+                    <p className='text-center py-4'>
+                        Already have an account?
+                        <span className='underline'>
+                            <Link to='signin' > Sign In
+                            </Link>
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
